c71c9dd103daaf2a2df4349fd3f47d1191846531
sag7221 <toshilpa@gmail.com>
2022-12-07 15:17:27 -0800

    Added style and color

--- Mapping_GeoJSON_Linestrings/logic.js
+++ Mapping_GeoJSON_Linestrings/logic.js
@@ -25,8 +25,33 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/sag7221/Mapping_Earthquakes/main/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    color: "#ffffa1",
+    weight: 2,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: "
+        + feature.properties.dst + "</h3>")
+    }
+  }).addTo(map);
+});
+
+
